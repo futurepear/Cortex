c331034584498d21d380b5765545f28c0b9e194f
mybiggestfear <chendy@oregonstate.edu>
2026-05-02 21:09:28 -0700

    modify DiscordItem to only accept discord message

--- frontend/src/components/items/ItemTemplate.tsx
+++ frontend/src/components/items/ItemTemplate.tsx
@@ -2,7 +2,7 @@ interface ItemProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-export default function Box({ className = "", ...props }: ItemProps) {
+export default function Item({ className = "", ...props }: ItemProps) {
   return (
     <div className={`para-box ${className}`} {...props}>
       <div className="font-mono text-fg uppercase tracking-widest text-xs">
